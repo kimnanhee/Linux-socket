d1354dc944e73af930fa1d3a9dbc153abb0053a6
kimnanhee <nanhee@user.localdomain>
2020-09-08 13:50:28 +0900

    [Add] listen() function

--- hello_server.c
+++ hello_server.c
@@ -33,6 +33,9 @@ int main(int argc, char *argv[])
 	serv_addr.sin_port = htons(atoi(argv[1]));
 
 	if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+		error_handling("bind() error");
+	
+	if(listen(serv_sock, 5)==-1)
 		error_handling("listen() error");
 
 	clnt_addr_size = sizeof(clnt_addr);
